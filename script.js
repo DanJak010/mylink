ee823952797ada91646d1b7e895564e1c57bce86
DanJak010 <daniele.martini2705@gmail.com>
2022-04-13 15:49:24 +0200

    #1

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
-function openUrl(url, pagina) {
-    window.open(url, pagina)
-}
 
+var typed = new Typed(".typing", {
+    strings: ["Developer", "Programmer CSS-HTML-PY-JS"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    cursorChar: ' |',
+    smartBackspace: true,
+    loop: true
+});
